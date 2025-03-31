7491b206686bc5cb36dff6008c3f29cb29518d1b
rodis00 <happyekkov@gmail.com>
2025-03-31 10:13:53 +0200

    project init

---
+++ src/test/java/com/rodis00/atiperatask/AtiperaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodis00.atiperatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiperaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
